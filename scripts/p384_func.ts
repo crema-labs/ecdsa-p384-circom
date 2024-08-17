f578403e8a3717686a9c2d7a01e35a8f7dca69b3
ayman <aymanshaik1015@gmail.com>
2024-08-17 11:52:57 +0530

    chore: switch to p384

--- scripts/p384_func.ts
+++ scripts/p384_func.ts
@@ -1,22 +1,22 @@
 import elliptic from "elliptic";
 import { hexToBigInt, splitToWords } from "../src";
 
-const EC = new elliptic.ec("p256");
+const EC = new elliptic.ec("p384");
 
 const N = hexToBigInt(EC.curve.n.toString(16));
 const P = hexToBigInt(EC.curve.p.toString(16));
-const A = BigInt("115792089210356248762697446949407573530086143415290314195533631308867097853948");
-const B = BigInt("41058363725152142129326129780047268409114441015993725554835256314039467401291");
+const A = hexToBigInt(EC.curve.a.toString(16));
+const B = hexToBigInt(EC.curve.b.toString(16));
 
 console.log(`N: ${N}`);
 console.log(`P: ${P}`);
 console.log(`A: ${A}`);
 console.log(`B: ${B}`);
 
-const n_words = splitToWords(N, 43n, 6n);
-const p_words = splitToWords(P, 43n, 6n);
-const a_words = splitToWords(A, 43n, 6n);
-const b_words = splitToWords(B, 43n, 6n);
+const n_words = splitToWords(N, 48n, 8n);
+const p_words = splitToWords(P, 48n, 8n);
+const a_words = splitToWords(A, 48n, 8n);
+const b_words = splitToWords(B, 48n, 8n);
 
 console.log(`n_words: ${n_words}`);
 console.log(`p_words: ${p_words}`);
